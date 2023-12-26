6039ca752a780012324b9213d48e35fb62d55c56
itsteatv <rezashow60@gmail.com>
2023-12-26 10:05:38 +0330

    react router + react query setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,32 @@
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 0,
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
   return (
-    <div>
-      
-    </div>
-  )
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<p>AppLayout</p>}>
+            <Route index element={<Navigate replace to="products" />} />
+            <Route path="products" element={<p>products</p>} />
+            <Route path="products/:productsId" element={<p>productsId</p>} />
+          </Route>
+
+          <Route path="*" element={<p>PageNotFound</p>} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
