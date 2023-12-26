553cc8ec4b834a5e5ffe8343fe6b76589a584d52
itsteatv <rezashow60@gmail.com>
2023-12-26 10:48:15 +0330

    page not found component added

---
+++ client/src/components/UI/PageNotFound.tsx
@@ -0,0 +1,32 @@
+import { useNavigate } from "react-router-dom";
+
+function PageNotFound() {
+  const navigate = useNavigate();
+
+  const handleGoBackHome = () => {
+    navigate("/", { replace: true });
+  };
+
+  return (
+    <div className="grid h-screen place-content-center bg-white px-4">
+      <div className="text-center">
+        <h1 className="text-9xl font-black text-gray-200">404</h1>
+
+        <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Uh-oh!
+        </p>
+
+        <p className="mt-4 text-gray-500">We can't find that page.</p>
+
+        <div
+          onClick={handleGoBackHome}
+          className="mt-6 inline-block rounded bg-indigo-600 cursor-pointer px-5 py-3 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring"
+        >
+          Go Back Home
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PageNotFound;
