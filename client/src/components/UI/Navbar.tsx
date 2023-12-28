dd5d69a3213762d0ebd2c584d9952c57b0382699
itsteatv <rezashow60@gmail.com>
2023-12-28 20:51:48 +0330

    navbar links updated

--- client/src/components/UI/Navbar.tsx
+++ client/src/components/UI/Navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
   return (
     <>
       {/*<!-- Header --> */}
-      <header className="border-b-1 relative z-20 w-full border-b duration-300 border-slate-200 dark:bg-gray-800 bg-white/90 shadow-lg shadow-slate-700/5 after:absolute after:top-full after:left-0 after:z-10 after:block after:h-px after:w-full after:bg-slate-200 lg:border-slate-200 lg:backdrop-blur-sm lg:after:hidden">
+      <header className="border-b-1 relative z-20 w-full border-b border-slate-200 dark:bg-gray-800 bg-white/90 shadow-lg shadow-slate-700/5 after:absolute after:top-full after:left-0 after:z-10 after:block after:h-px after:w-full after:bg-slate-200 lg:border-slate-200 lg:backdrop-blur-sm lg:after:hidden">
         <div className="relative mx-auto max-w-full px-6 lg:max-w-5xl xl:max-w-7xl 2xl:max-w-[96rem]">
           <nav
             aria-label="main navigation"
@@ -63,14 +63,15 @@ function Navbar() {
                 </Link>
               </li>
               <li role="none" className="flex items-stretch">
-                <div
+                <Link
                   role="menuitem"
                   aria-current="page"
                   aria-haspopup="false"
                   className="flex items-center gap-2 py-4 text-emerald-500 transition-colors duration-300 hover:text-emerald-600 focus:text-emerald-600 focus:outline-none focus-visible:outline-none lg:px-8"
+                  to="dashboard"
                 >
                   <span>MERN E-COMMERCE</span>
-                </div>
+                </Link>
               </li>
               <li role="none" className="flex items-stretch">
                 <Link
