f0c223108636e3b409693ae2d5684584835b1d93
itsteatv <rezashow60@gmail.com>
2023-12-29 12:39:49 +0330

    skeleton loading added

--- client/src/components/pages/Products.tsx
+++ client/src/components/pages/Products.tsx
@@ -1,7 +1,8 @@
 import { useFetchProducts } from "../hooks/useFetchProducts";
 import Ratings from "../ui/Ratings";
+import SkeletonLoading from "../ui/SkeletonLoading";
 function Products() {
-  const { isError, isLoading, products } = useFetchProducts();
+  const { error, isLoading, products } = useFetchProducts();
 
   const formatPrice = (price: number) => {
     return new Intl.NumberFormat("en-US", {
@@ -14,36 +15,51 @@ function Products() {
   return (
     <>
       <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto min-h-screen flex items-center flex-col justify-center">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
-          {products.map((product) => (
-            <div
-              key={product._id}
-              className="bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700"
-            >
-              <div className="p-4 md:p-6 dark:text-white">
-                {product.name}
-                <p className="mt-3 sm:mt-6 text-base text-gray-800 md:text-xl dark:text-white">
-                  <em>
-                    " I'm absolutely floored by the level of care and attention
-                    to detail the team at HS have put into this theme and for
-                    one can guarantee that I will be a return customer. "
-                  </em>
-                </p>
+        {isLoading ? (
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+            {products.map((product) => (
+              <div key={product._id}>
+                <SkeletonLoading />
               </div>
-              <div className="p-4 rounded-b-xl md:px-6">
-                <h3 className=">=445px:flex >=445px:justify-center text-sm font-semibold text-gray-800 sm:text-base dark:text-gray-200">
-                  {formatPrice(product.price)}
-                </h3>
-                <div className="dark:text-slate-500 italic text-sx mt-1 >=445px:text-center">
-                  {product.company} company
+            ))}
+          </div>
+        ) : error ? (
+          <div className="text-red-500 text-xl">
+            Error fetching products. Please try again later.
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+            {products.map((product) => (
+              <div
+                key={product._id}
+                className="bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700"
+              >
+                <div className="p-4 md:p-6 dark:text-white">
+                  {product.name}
+                  <p className="mt-3 sm:mt-6 text-base text-gray-800 md:text-xl dark:text-white">
+                    <em>
+                      " I'm absolutely floored by the level of care and
+                      attention to detail the team at HS have put into this
+                      theme and for one can guarantee that I will be a return
+                      customer. "
+                    </em>
+                  </p>
                 </div>
-                <div className="flex text-sm items-center text-gray-500 mt-1 >=445px:justify-center">
-                  <Ratings ratings={product.rating} />
+                <div className="p-4 rounded-b-xl md:px-6">
+                  <h3 className=">=445px:flex >=445px:justify-center text-sm font-semibold text-gray-800 sm:text-base dark:text-gray-200">
+                    {formatPrice(product.price)}
+                  </h3>
+                  <div className="dark:text-slate-500 italic text-sx mt-1 >=445px:text-center">
+                    {product.company} company
+                  </div>
+                  <div className="flex text-sm items-center text-gray-500 mt-1 >=445px:justify-center">
+                    <Ratings ratings={product.rating} />
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
