c957dc51e4a71751b1434ec57d1442aa284d2394
itsteatv <rezashow60@gmail.com>
2023-12-31 10:18:58 +0330

    fetch products hook updated (total result)

--- client/src/components/hooks/useFetchProducts.tsx
+++ client/src/components/hooks/useFetchProducts.tsx
@@ -3,14 +3,13 @@ import { FetchProducts } from "../api/FetchProducts";
 import { ProductsResponse } from "../utils/Interfaces";
 
 export function useFetchProducts(): ProductsResponse {
-  const {
-    error,
-    isLoading,
-    data: products,
-  } = useQuery<ProductsResponse>({
+  const { error, isLoading, data } = useQuery<ProductsResponse>({
     queryKey: ["products"],
     queryFn: FetchProducts,
   });
 
-  return { error, isLoading, products: products?.products || [] };
+  const totalResult = data?.totalResult || 0;
+  const products = data?.products || [];
+
+  return { error, isLoading, products, totalResult };
 }
