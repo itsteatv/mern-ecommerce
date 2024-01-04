bb2819372ec6c49065716caf70393a2563bb45fa
itsteatv <rezashow60@gmail.com>
2024-01-05 02:15:10 +0330

    refactor: Enhance Products, Pagination & search functionality

--- client/src/components/UI/Pagination.tsx
+++ client/src/components/UI/Pagination.tsx
@@ -1,11 +1,20 @@
 import { useSearchParams } from "react-router-dom";
 import { PAGE_SIZES } from "../utils/PageSize";
+import { Product } from "../utils/Interfaces";
 
 type PaginationProps = {
   totalResult: number;
+  filteredProductsLength: number;
+  filteredProducts: Product[];
+  searchTerm: string;
 };
 
-function Pagination({ totalResult }: PaginationProps) {
+function Pagination({
+  totalResult,
+  filteredProductsLength,
+  filteredProducts,
+  searchTerm,
+}: PaginationProps) {
   const [searchParams, setSearchParams] = useSearchParams();
   const currentPage = Number(searchParams.get("page")) || 1;
 
@@ -27,71 +36,149 @@ function Pagination({ totalResult }: PaginationProps) {
   const startResult = (currentPage - 1) * pageSize + 1;
   const endResult = Math.min(currentPage * pageSize, totalResult);
 
+  const searchedProducts = filteredProducts.filter((product) =>
+    product.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  // FOR FILTERED PRODUCTS
+  const totalFilteredPages = filteredProductsLength;
+  const startFilteredResult = searchedProducts.length;
+  const endFilteredResult = Math.min(currentPage * pageSize, pageSize);
+
+  const endResultOnPage = Math.min(currentPage * pageSize, totalResult);
+  console.log("End Filtered Result:", endResultOnPage);
+
   return (
     <>
-      <nav className="flex items-center gap-x-1 mt-10">
-        <button
-          type="button"
-          className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:cursor-not-allowed dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
-          onClick={() => handlePageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-        >
-          <svg
-            className="flex-shrink-0 w-3.5 h-3.5"
-            xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
+      {totalResult ? (
+        <nav className="flex items-center gap-x-1 mt-10">
+          <button
+            type="button"
+            className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:cursor-not-allowed dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
           >
-            <path d="m15 18-6-6 6-6" />
-          </svg>
-          <span>Previous</span>
-        </button>
-        <div className="flex items-center gap-x-1">
-          {Array.from({ length: totalPages }, (_, index) => (
-            <button
-              key={index + 1}
-              type="button"
-              className={`min-h-[38px] min-w-[38px] flex justify-center items-center ${
-                index + 1 === currentPage
-                  ? "bg-gray-200 text-gray-800"
-                  : "text-gray-800 hover:bg-gray-100"
-              } py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10`}
-              onClick={() => handlePageChange(index + 1)}
-              disabled={index + 1 === currentPage}
+            <svg
+              className="flex-shrink-0 w-3.5 h-3.5"
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
             >
-              {index + 1}
-            </button>
-          ))}
-        </div>
-        <button
-          type="button"
-          className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:cursor-not-allowed dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
-          onClick={() => handlePageChange(currentPage + 1)}
-          disabled={currentPage === totalPages}
-        >
-          <span>Next</span>
-          <svg
-            className="flex-shrink-0 w-3.5 h-3.5"
-            xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
+              <path d="m15 18-6-6 6-6" />
+            </svg>
+            <span>Previous</span>
+          </button>
+          <div className="flex items-center gap-x-1">
+            {Array.from({ length: totalPages }, (_, index) => (
+              <button
+                key={index + 1}
+                type="button"
+                className={`min-h-[38px] min-w-[38px] flex justify-center items-center ${
+                  index + 1 === currentPage
+                    ? "bg-gray-200 text-gray-800"
+                    : "text-gray-800 hover:bg-gray-100"
+                } py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10`}
+                onClick={() => handlePageChange(index + 1)}
+                disabled={index + 1 === currentPage}
+              >
+                {index + 1}
+              </button>
+            ))}
+          </div>
+          <button
+            type="button"
+            className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:cursor-not-allowed dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
           >
-            <path d="m9 18 6-6-6-6" />
-          </svg>
-        </button>
-      </nav>
+            <span>Next</span>
+            <svg
+              className="flex-shrink-0 w-3.5 h-3.5"
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="m9 18 6-6-6-6" />
+            </svg>
+          </button>
+        </nav>
+      ) : (
+        <nav className="flex items-center gap-x-1 mt-10">
+          <button
+            type="button"
+            className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:cursor-not-allowed dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            <svg
+              className="flex-shrink-0 w-3.5 h-3.5"
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="m15 18-6-6 6-6" />
+            </svg>
+            <span>Previous</span>
+          </button>
+          <div className="flex items-center gap-x-1">
+            {Array.from({ length: totalFilteredPages }, (_, index) => (
+              <button
+                key={index + 1}
+                type="button"
+                className={`min-h-[38px] min-w-[38px] flex justify-center items-center ${
+                  index + 1 === currentPage
+                    ? "bg-gray-200 text-gray-800"
+                    : "text-gray-800 hover:bg-gray-100"
+                } py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10`}
+                onClick={() => handlePageChange(index + 1)}
+                disabled={index + 1 === currentPage}
+              >
+                {index * pageSize + 1} -{" "}
+                {Math.min((index + 1) * pageSize, totalResult)}
+              </button>
+            ))}
+          </div>
+          <button
+            type="button"
+            className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:cursor-not-allowed dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalFilteredPages}
+          >
+            <span>Next</span>
+            <svg
+              className="flex-shrink-0 w-3.5 h-3.5"
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="m9 18 6-6-6-6" />
+            </svg>
+          </button>
+        </nav>
+      )}
 
       {/* PAGE SIZE DROPDOWN */}
       <div className="flex items-center my-1">
@@ -112,21 +199,46 @@ function Pagination({ totalResult }: PaginationProps) {
         </select>
       </div>
 
-      <p className="italic text-gray-500 text-sm mt-2">
-        Showing{" "}
-        <span className="dark:text-white text-gray-800 font-bold">
-          {startResult}
-        </span>{" "}
-        to{" "}
-        <span className="dark:text-white text-gray-800 font-bold">
-          {endResult}
-        </span>{" "}
-        of{" "}
-        <span className="dark:text-white text-gray-800 font-bold">
-          {totalResult}
-        </span>{" "}
-        results
-      </p>
+      {searchTerm ? (
+        <p className="italic text-gray-500 text-sm mt-2">
+          {startFilteredResult > 0 ? (
+            <>
+              We found{" "}
+              <span className="dark:text-white text-gray-800 font-bold">
+                {startFilteredResult}
+              </span>{" "}
+              result{startFilteredResult !== 1 ? "s" : ""}{" "}
+            </>
+          ) : (
+            "No results found"
+          )}{" "}
+          <span className="italic text-gray-500 text-sm mt-2">of </span>{" "}
+          <span className="dark:text-white text-gray-800 font-bold">
+            {endResultOnPage - startResult + 1}
+          </span>
+          <span className="ml-1">
+            result{endResultOnPage - startResult + 1 !== 1 && "s"} in this page
+          </span>
+        </p>
+      ) : (
+        <div>
+          <p className="italic text-gray-500 text-sm mt-2">
+            Showing{" "}
+            <span className="dark:text-white text-gray-800 font-bold">
+              {startResult}
+            </span>{" "}
+            to{" "}
+            <span className="dark:text-white text-gray-800 font-bold">
+              {endResult}
+            </span>{" "}
+            of{" "}
+            <span className="dark:text-white text-gray-800 font-bold">
+              {totalResult}
+            </span>{" "}
+            results
+          </p>
+        </div>
+      )}
     </>
   );
 }
--- client/src/components/pages/Products.tsx
+++ client/src/components/pages/Products.tsx
@@ -4,11 +4,24 @@ import Ratings from "../ui/Ratings";
 import SkeletonLoading from "../ui/SkeletonLoading";
 import Pagination from "../ui/Pagination";
 import SearchInput from "../ui/SearchInput";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Product } from "../utils/Interfaces";
 
 function Products() {
   const { products, totalResult, error, isLoading } = useFetchProducts();
   const [searchTerm, setSearchTerm] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    if (searchTerm) {
+      const filteredResults = products.filter((product) =>
+        product.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredProducts(filteredResults);
+    } else {
+      setFilteredProducts([]);
+    }
+  }, [products, searchTerm]);
 
   console.log(products);
   console.log(totalResult);
@@ -20,10 +33,6 @@ function Products() {
       minimumFractionDigits: 2,
     }).format(price);
   };
-
-  const filteredProducts = products.filter((product) =>
-    product.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
 
   console.log(filteredProducts);
 
@@ -45,6 +54,8 @@ function Products() {
     return <ErrorFallback errorMessage={error?.message || "No Data Found"} />;
   }
 
+  const displayProducts = searchTerm ? filteredProducts : products;
+
   return (
     <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto min-h-screen flex items-center flex-col justify-center">
       <SearchInput
@@ -53,44 +64,46 @@ function Products() {
         placeholder="Enter the product name"
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-      {filteredProducts.length === 0 ? (
+      {displayProducts.length === 0 ? (
         <ErrorFallback errorMessage="No Result Found" />
       ) : (
-        <>
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
-            {filteredProducts.map((product) => (
-              <div
-                key={product._id}
-                className="bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700"
-              >
-                <div className="p-4 md:p-6 dark:text-white">
-                  {product.name}
-                  <p className="mt-3 sm:mt-6 text-base text-gray-800 md:text-xl dark:text-white">
-                    <em>
-                      " I'm absolutely floored by the level of care and
-                      attention to detail the team at HS have put into this
-                      theme and for one can guarantee that I will be a return
-                      customer. "
-                    </em>
-                  </p>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+          {displayProducts.map((product) => (
+            <div
+              key={product._id}
+              className="bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700"
+            >
+              <div className="p-4 md:p-6 dark:text-white">
+                {product.name}
+                <p className="mt-3 sm:mt-6 text-base text-gray-800 md:text-xl dark:text-white">
+                  <em>
+                    " I'm absolutely floored by the level of care and attention
+                    to detail the team at HS have put into this theme and for
+                    one can guarantee that I will be a return customer. "
+                  </em>
+                </p>
+              </div>
+              <div className="p-4 rounded-b-xl md:px-6">
+                <h3 className=">=445px:flex >=445px:justify-center text-sm font-semibold text-gray-800 sm:text-base dark:text-gray-200">
+                  {formatPrice(product.price)}
+                </h3>
+                <div className="dark:text-slate-500 italic text-sx mt-1 >=445px:text-center">
+                  {product.company} company
                 </div>
-                <div className="p-4 rounded-b-xl md:px-6">
-                  <h3 className=">=445px:flex >=445px:justify-center text-sm font-semibold text-gray-800 sm:text-base dark:text-gray-200">
-                    {formatPrice(product.price)}
-                  </h3>
-                  <div className="dark:text-slate-500 italic text-sx mt-1 >=445px:text-center">
-                    {product.company} company
-                  </div>
-                  <div className="flex text-sm items-center text-gray-500 mt-1 >=445px:justify-center">
-                    <Ratings ratings={product.rating} />
-                  </div>
+                <div className="flex text-sm items-center text-gray-500 mt-1 >=445px:justify-center">
+                  <Ratings ratings={product.rating} />
                 </div>
               </div>
-            ))}
-          </div>
-          <Pagination totalResult={totalResult} />
-        </>
+            </div>
+          ))}
+        </div>
       )}
+      <Pagination
+        totalResult={totalResult}
+        filteredProductsLength={filteredProducts.length}
+        filteredProducts={filteredProducts}
+        searchTerm={searchTerm}
+      />
     </div>
   );
 }
