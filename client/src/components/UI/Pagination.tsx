ac768563198979bf743f36119a9b5abe622761eb
itsteatv <rezashow60@gmail.com>
2024-01-08 20:43:43 +0330

    refactor: Enhance Pagination component with improved readability and maintainability

--- client/src/components/UI/Pagination.tsx
+++ client/src/components/UI/Pagination.tsx
@@ -41,145 +41,77 @@ function Pagination({
     product.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
   // FOR FILTERED PRODUCTS
-  const totalFilteredPages = filteredProductsLength;
+  // const totalFilteredPages = filteredProductsLength;
+  // const endFilteredResult = Math.min(currentPage * pageSize, pageSize);
   const startFilteredResult = searchedProducts.length;
-  const endFilteredResult = Math.min(currentPage * pageSize, pageSize);
 
   const endResultOnPage = Math.min(currentPage * pageSize, totalResult);
-  console.log("End Filtered Result:", endResultOnPage);
 
   return (
     <>
-      {totalResult ? (
-        <nav className="flex items-center gap-x-1 mt-10 >=340px:flex-col">
-          <button
-            type="button"
-            className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:cursor-not-allowed dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
-            onClick={() => handlePageChange(currentPage - 1)}
-            disabled={currentPage === 1}
+      <nav className="flex items-center gap-x-1 mt-10 >=340px:flex-col">
+        <button
+          type="button"
+          className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:cursor-not-allowed dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          <svg
+            className="flex-shrink-0 w-3.5 h-3.5"
+            xmlns="http://www.w3.org/2000/svg"
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth={2}
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
-            <svg
-              className="flex-shrink-0 w-3.5 h-3.5"
-              xmlns="http://www.w3.org/2000/svg"
-              width={24}
-              height={24}
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={2}
-              strokeLinecap="round"
-              strokeLinejoin="round"
+            <path d="m15 18-6-6 6-6" />
+          </svg>
+          <span>Previous</span>
+        </button>
+        <div className="flex items-center gap-x-1 >=340px:flex-col">
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              key={index + 1}
+              type="button"
+              className={`min-h-[38px] min-w-[38px] flex justify-center items-center ${
+                index + 1 === currentPage
+                  ? "bg-gray-200 text-gray-800"
+                  : "text-gray-800 hover:bg-gray-100"
+              } py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10`}
+              onClick={() => handlePageChange(index + 1)}
+              disabled={index + 1 === currentPage}
             >
-              <path d="m15 18-6-6 6-6" />
-            </svg>
-            <span>Previous</span>
-          </button>
-          <div className="flex items-center gap-x-1 >=340px:flex-col">
-            {Array.from({ length: totalPages }, (_, index) => (
-              <button
-                key={index + 1}
-                type="button"
-                className={`min-h-[38px] min-w-[38px] flex justify-center items-center ${
-                  index + 1 === currentPage
-                    ? "bg-gray-200 text-gray-800"
-                    : "text-gray-800 hover:bg-gray-100"
-                } py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10`}
-                onClick={() => handlePageChange(index + 1)}
-                disabled={index + 1 === currentPage}
-              >
-                {index + 1}
-              </button>
-            ))}
-          </div>
-          <button
-            type="button"
-            className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:cursor-not-allowed dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
-            onClick={() => handlePageChange(currentPage + 1)}
-            disabled={currentPage === totalPages}
+              {index + 1}
+            </button>
+          ))}
+        </div>
+        <button
+          type="button"
+          className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:cursor-not-allowed dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          <span>Next</span>
+          <svg
+            className="flex-shrink-0 w-3.5 h-3.5"
+            xmlns="http://www.w3.org/2000/svg"
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth={2}
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
-            <span>Next</span>
-            <svg
-              className="flex-shrink-0 w-3.5 h-3.5"
-              xmlns="http://www.w3.org/2000/svg"
-              width={24}
-              height={24}
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={2}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="m9 18 6-6-6-6" />
-            </svg>
-          </button>
-        </nav>
-      ) : (
-        <nav className="flex items-center gap-x-1 mt-10">
-          <button
-            type="button"
-            className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:cursor-not-allowed dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
-            onClick={() => handlePageChange(currentPage - 1)}
-            disabled={currentPage === 1}
-          >
-            <svg
-              className="flex-shrink-0 w-3.5 h-3.5"
-              xmlns="http://www.w3.org/2000/svg"
-              width={24}
-              height={24}
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={2}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="m15 18-6-6 6-6" />
-            </svg>
-            <span>Previous</span>
-          </button>
-          <div className="flex items-center gap-x-1">
-            {Array.from({ length: totalFilteredPages }, (_, index) => (
-              <button
-                key={index + 1}
-                type="button"
-                className={`min-h-[38px] min-w-[38px] flex justify-center items-center ${
-                  index + 1 === currentPage
-                    ? "bg-gray-200 text-gray-800"
-                    : "text-gray-800 hover:bg-gray-100"
-                } py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10`}
-                onClick={() => handlePageChange(index + 1)}
-                disabled={index + 1 === currentPage}
-              >
-                {index * pageSize + 1} -{" "}
-                {Math.min((index + 1) * pageSize, totalResult)}
-              </button>
-            ))}
-          </div>
-          <button
-            type="button"
-            className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:cursor-not-allowed dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
-            onClick={() => handlePageChange(currentPage + 1)}
-            disabled={currentPage === totalFilteredPages}
-          >
-            <span>Next</span>
-            <svg
-              className="flex-shrink-0 w-3.5 h-3.5"
-              xmlns="http://www.w3.org/2000/svg"
-              width={24}
-              height={24}
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={2}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="m9 18 6-6-6-6" />
-            </svg>
-          </button>
-        </nav>
-      )}
+            <path d="m9 18 6-6-6-6" />
+          </svg>
+        </button>
+      </nav>
 
       <PageSizeDropdown
         value={pageSize}
